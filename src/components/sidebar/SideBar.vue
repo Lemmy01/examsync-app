a688725ce7d0db8e553e051b3e58228817397dc2
Vasile <vasilicalutalsv@gmail.com>
2024-11-13 15:47:28 +0200

    am schimbat numele la sidebar

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,5 +1,4 @@
 <template>
-
       <v-navigation-drawer  v-model="drawer" app>
         <v-list>
           <v-list-item
@@ -12,14 +11,11 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" color="primary" title="Login" value="Login" to="/"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="View Teachers" value="view_teachers"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="View Exams" value="view_exams"></v-list-item> 
+           <v-list-item prepend-icon="mdi-folder" title="Login" value="Login" to="/"></v-list-item>
         </v-list>
       </v-navigation-drawer>
-
- 
-  
 </template>
 
 <script>

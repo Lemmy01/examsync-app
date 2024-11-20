e268b1ecc7cc97f3b32f5c4b631c893766a91a7c
Vasile <vasilicalutalsv@gmail.com>
2024-11-20 19:58:36 +0200

    am schimbat url la backend

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ipgrupa4echipa1-b4acchb9bfcnfzfc.francecentral-01.azurewebsites.net', // Replace with your API's base URL
+  baseURL: 'https://ipgrupa4echipa1-b4acchb9bfcnfzfc.northeurope-01.azurewebsites.net/', // Replace with your API's base URL
   headers: {
     'Content-Type': 'application/json',
   },
